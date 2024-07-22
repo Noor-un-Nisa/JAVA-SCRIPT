6c4ed5264cbc44354bc3536d0457cc7b9a941263
Noor-un-Nisa <noornisa784@gmail.com>
2024-07-22 13:02:47 +0000

    datatype conversion

---
+++ Basics_JS/03_conversion.js
@@ -0,0 +1,34 @@
+//Converting Number data type to other data types
+// NaN = NOT A NUMBER
+let aged=19
+console.log(typeof aged);
+console.log(aged);
+let convertedType=Boolean(aged)
+console.log(typeof convertedType);
+console.log(convertedType);
+let convertType=String(aged)
+console.log(typeof convertType);
+console.log(convertType);
+let convert_Type=BigInt(aged)
+console.log(typeof convert_Type);
+console.log(convert_Type); // 19n n donates the bigInt
+// Converting String data type to other data types
+let Name="noor"
+console.log(typeof Name);
+console.log(Name);
+let NAME=Number (Name)
+console.log(typeof NAME);
+console.log(NAME);
+let NAMe=Boolean (Name)
+console.log(typeof NAMe);
+console.log(NAMe);
+// Converting Boolean data type into other data types
+let LoggedIn=true
+console.log(typeof LoggedIn);
+console.log(LoggedIn);
+let loggedIn=Number (LoggedIn)
+console.log(typeof loggedIn);
+console.log(loggedIn);
+let Logged_in=String (LoggedIn)
+console.log(typeof Logged_in);
+console.log(Logged_in);
