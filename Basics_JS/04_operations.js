b596c44da16d1af2f4f9a5af3d6e817a7ff3388a
Noor-un-Nisa <noornisa784@gmail.com>
2024-07-23 14:38:34 +0000

    23 july

---
+++ Basics_JS/04_operations.js
@@ -0,0 +1,20 @@
+ // 2+2 => addition
+// 2-2 => subtraction
+// 2*2  => multiplication
+// 2*3 => 2power3
+// 4/2 => division
+//5%2 => modulus/remainder
+
+// adddition of string
+let str1 = "noor "
+let str2 = "un "
+let str3 = "nisa"
+let str = str1 + str2 +str3
+console.log(str);
+
+// addition of string and number
+
+console.log("mangoes" + 10 )
+console.log(10 + "mangoes")
+console.log(1 + 2 + "7")
+console.log("7" + 2 +1)
---
+++ Basics_JS/05_comparison.js
@@ -0,0 +1,40 @@
+// ** IN JAVA SCRIPT EQUALITY AND COMPARISON OPERATORS WORK DIFFERENTLY
+// Comparison of same data type
+console.log(2>4)
+console.log(2>=4)
+console.log(2<4)
+console.log(2<=4)
+console.log(2==4)
+console.log(2!=4)
+console.log(true>false)
+console.log("2">"5")
+
+// Comparison of differenet datatypes
+
+console.log(true>5)
+console.log(5>="3")
+console.log(false=="0")
+
+// **FOLLOWING COMPARISONS ARE AVOIDED
+// Comparison of null datatype
+console.log(null>0)// false
+console.log(null>=0)// true
+console.log(null<0) // false
+console.log(null<=0)// true
+console.log(null==0)//false
+console.log(null!=0)// true
+// in comparison null is converted to a number "0"
+
+// Comparison of undefined
+
+console.log(undefined>0)// false
+console.log(undefined>=0)// false
+console.log(undefined<0)// false
+console.log(undefined<=0)// false
+console.log(undefined==0)// false
+console.log(undefined!=0)// true
+
+//*** === strict check. checks the datatype of  variable. no conversion of datatype in it
+// == conversion occurs
+console.log("2" === 2)// false
+console.log("2" == 2)// true
