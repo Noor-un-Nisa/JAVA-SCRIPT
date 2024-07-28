818362cf8c33217cac85135af7d1ee42f5861019
Noor-un-Nisa <noornisa784@gmail.com>
2024-07-28 08:00:58 +0000

    datatypes

---
+++ 01_Basics_JS/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all code as the newer version of JS
+// alert("HELLO") error because it is the method of window object 
+//  window alert () method instructs thhe browser to display a message and a OK box while  console() is  used to just display some output on the console.
+// code readiability should be high
+// DATA TYPES:
+//1. number => range is from 2 to the power of 52
+let age=19
+//2. string => in double quotes " or in single quotes ' but double quotes is preferred
+let name="noor"
+//3. bigInt => for  number greater the 2^52
+//4. null => it is a standalone value means it is empty
+let city=null;
+//5. undefined => value is not defined
+let country;
+//6. boolean => true=1 / false=0 
+let loggedIn=true
+//7. symbol => to find uniqueness
+//8. object
+
+console.log(typeof loggedIn ) //  typeof is used to find the data type of a variable. Syntax: console.log(typeof variable name)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
