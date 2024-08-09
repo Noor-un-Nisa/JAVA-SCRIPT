6d3c3ed61c4ad9b33159e6c8e118b3e16dadd3a6
Noor-un-Nisa <noornisa784@gmail.com>
2024-08-09 14:22:54 +0000

    Switch Statements

---
+++ 05_Logic_Control_JS/02_SwitchStructure.js
@@ -0,0 +1,44 @@
+// agr bresk statement ni lgaty toh jon sa case match ho jy toh phir us break ke neechy ka sab print hota ha siway default ke
+
+let month = 1
+switch (month) {
+    case 1:
+        console.log(`January`);
+        break;
+    case 2:
+        console.log(`February`);
+        break;
+    case 3:
+        console.log(`March`);
+        break;
+    case 4:
+        console.log(`April`);
+        break;
+    case 5:
+        console.log(`May`);
+        break;
+    case 6:
+        console.log(`June`);
+        break;
+    case 7:
+        console.log(`July`);
+        break;
+    case 8:
+        console.log(`August`);
+        break;
+    case 9:
+        console.log(`September`);
+        break;
+    case 10:
+        console.log(`October`);
+        break;
+    case 11:
+        console.log(`November`);
+        break;
+    case 12:
+        console.log(`December`);
+        break;
+    default:
+        console.log(`Invalid number of month`);
+        break;
+}
