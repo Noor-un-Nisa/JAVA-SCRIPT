f1fc0a3b048c1d877f336b1db95fac2c26c47148
Noor-un-Nisa <noornisa784@gmail.com>
2024-08-12 14:14:26 +0000

    do while loop

---
+++ 06_Loops_Iterator_JS/03_DoWhileLoop.js
@@ -0,0 +1,13 @@
+// DO WHILE LOOP
+
+let score = 0
+do {
+    console.log(`Score ${score}`);
+    score += 5
+} while (score<=50);
+
+
+let marks = 10
+do {
+    console.log(`keep going`);
+} while (marks>10);
