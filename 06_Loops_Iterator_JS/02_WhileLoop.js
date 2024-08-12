f0fe3d217f4437bdb0b99708781767ef2809c3a8
Noor-un-Nisa <noornisa784@gmail.com>
2024-08-12 14:09:40 +0000

    While Loop

---
+++ 06_Loops_Iterator_JS/02_WhileLoop.js
@@ -0,0 +1,17 @@
+// WHILE LOOP
+
+let sum = 0
+
+while (sum<20) {
+    sum +=4
+    console.log(`sum is ${sum}`);
+}
+
+// ARRAY AND WHILE LOOP
+
+const Avengers = ['batman','ironman','thor','spiderman']
+let length=0
+while(length<Avengers.length){
+    console.log(`${length+1} ${Avengers[length]}`);
+    length++
+}
